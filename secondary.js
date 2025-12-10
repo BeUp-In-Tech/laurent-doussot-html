813d43866459ef5830daad7542d6648af9b44939
arinahuque23 <arinahuqe23@gmail.com>
2025-12-10 19:04:47 +0600

    laurent-doussot-html landing

--- secondary.js
+++ secondary.js
@@ -99,3 +99,21 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll(); // initial load
+
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
